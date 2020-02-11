9b755341218606b0bd2097ad31fd93e0c954abf6
Scott Smith <scthsm@outlook.com>
2020-02-11 15:00:27 -0700

    up

--- js/index.js
+++ js/index.js
@@ -1,1 +1,62 @@
 // Your code goes here
+
+const images = document.querySelectorAll("img");
+const buttons = document.querySelectorAll(".btn");
+const destinations = document.querySelectorAll(".destination");
+images[0].addEventListener("mouseenter", () => {
+    images[0].style.transform = "scale(1.2)";
+    images[0].style.transition = "transform 0.3s";
+  });
+
+  images[0].addEventListener("mouseleave", () => {
+    images[0].style.transform = "scale(1)";
+  });
+
+  images[1].addEventListener("dblclick", () => {
+    images[1].src = "img/adventure2.jpg"
+    images[1].style.transition = "transform 1s";
+  });
+
+  images[1].addEventListener("mouseleave", () => {
+    images[1].src = "img/adventure.jpg"
+    images[1].style.transition = "transform 1s";
+  });
+
+  images[2].addEventListener("mouseenter", () => {
+    images[2].style.transform = "scale(1.2)";
+    images[2].style.transition = "transform 0.3s";
+  });
+  images[2].addEventListener("dblclick", () => {
+    images[2].style.transform = "scale(4)";
+    images[2].style.transition = "transform 0.3s";
+  });
+  images[2].addEventListener("click", () => {
+    images[2].style.transform = "scale(1)";
+    images[2].style.transition = "transform 0.3s";
+  });
+  images[2].addEventListener("mouseleave", () => {
+    images[2].style.transform = "scale(1)";
+    images[2].style.transition = "transform 0.3s";
+  });
+
+buttons[0].addEventListener("click", () => {
+    buttons[0].textContent = "Unavailable";
+    buttons[0].style.transition = "transform 1s";
+    buttons[0].style.backgroundColor = "red";
+    destinations[0].classList.add("error");
+  });
+
+  buttons[1].addEventListener("click", () => {
+    buttons[1].textContent = "Nope";
+    buttons[1].style.transition = "transform 1s";
+    buttons[1].style.backgroundColor = "red";
+    destinations[1].classList.add("error");
+  });
+
+  buttons[2].addEventListener("click", () => {
+    buttons[2].textContent = "Nada!";
+    buttons[2].style.transition = "transform 1s";
+    buttons[2].style.backgroundColor = "pink";
+    destinations[2].classList.add("error");
+  });
+
